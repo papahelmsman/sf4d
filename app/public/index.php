2440681bd1e536b8eb612da91fc9873f07a42ce9
papahelmsman <papahelmsman@gmail.com>
2019-09-09 00:35:57 +0600

    Docker Config 08-09-2019

--- app/public/index.php
+++ app/public/index.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Congrats!</title>
-</head>
-<body>
-    <h1>Congratulations! Your development environment is ready to use!</h1>
-</body>
-</html>
+<?php
+
+echo 'Congrats! It works!';
